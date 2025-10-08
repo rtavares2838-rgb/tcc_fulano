634c41e5f1670ca0e7887d2d895fed5a36048dd7
RenanTavares <rtavares2838@gmail.com>
2025-10-08 09:32:14 -0300

    v2.0

--- atualizador.js
+++
@@ -1,8 +0,0 @@
-setInterval(() => {
-    fetch('dados.json')
-        .then(res => res.json())
-        .then(data => {
-            document.getElementById('senha').textContent = data.senha;
-            document.getElementById('guiche').textContent = data.guiche;
-        });
-}, 1000); // Atualiza a cada 2 segundos
